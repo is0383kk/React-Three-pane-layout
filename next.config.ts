5625f5bf9d9d4be3742aeb7bef7776bc0ce13c21
is0383kk <is0383kk@gmail.com>
2025-06-06 18:44:32 +0900

    [fix] next.config

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? "/React-Three-pane-layout" : "",
-  assetPrefix: isProd ? "/React-Three-pane-layout/" : "",
+  basePath: isProd ? "/React-shadcn-pane-layouts" : "",
+  assetPrefix: isProd ? "/React-shadcn-pane-layouts/" : "",
   images: {
     unoptimized: true,
   },
