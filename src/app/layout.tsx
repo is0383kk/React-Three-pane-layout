2f0fa7b4e039c48778fc2022892bfbc1bd9eb808
is0383kk <is0383kk@gmail.com>
2025-06-10 18:56:44 +0900

    [fix] layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,38 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Your App",
-  description: "Your app description",
+  title: "React-shadcn-pane-layouts",
+  description:
+    "React-shadcn-pane-layouts is a layout component for React applications using shadcn-ui.",
+  openGraph: {
+    title: "React-shadcn-pane-layouts",
+    description:
+      "React-shadcn-pane-layouts is a layout component for React applications using shadcn-ui.",
+    url: "https://is0383kk.github.io/React-shadcn-pane-layouts/",
+    siteName: "React-shadcn-pane-layouts",
+    images: [
+      {
+        url: "https://is0383kk.github.io/React-shadcn-pane-layouts/ogp.png",
+        width: 1200,
+        height: 630,
+        alt: "React-shadcn-pane-layouts is a layout component for React applications using shadcn-ui.",
+      },
+    ],
+    locale: "ja_JP",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@is0383kk",
+    images: ["https://is0383kk.github.io/React-shadcn-pane-layouts/ogp.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
+  authors: [{ name: "is0383kk", url: "https://github.com/is0383kk" }],
+  creator: "is0383kk",
 };
 
 export default function RootLayout({
